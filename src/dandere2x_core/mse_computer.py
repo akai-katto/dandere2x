d62b96f656ea2be1a26e0984bb50e4c4a209be69
Tyler <tylerszeto@gmail.com>
2019-05-08 22:01:04 -0700

    quality settings

--- src/dandere2x_core/mse_computer.py
+++ src/dandere2x_core/mse_computer.py
@@ -7,10 +7,11 @@ def compress_frames(context: Context):
     inputs_dir = context.input_frames_dir
     frame_count = context.frame_count
     compressed_dir = context.compressed_dir
+    quality_low = context.quality_low
 
     for x in range(1, frame_count):
         frame = Frame()
 
         frame.load_from_string(inputs_dir + "frame" + str(x) + ".jpg")
-        frame.save_image_quality(compressed_dir + str(x) + ".jpg", 65)
+        frame.save_image_quality(compressed_dir + str(x) + ".jpg", quality_low)
 
