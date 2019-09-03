5736ff9ec56dbf9e16986b7a5771a94b5177f9b5
Tyler <tylerszeto@gmail.com>
2019-09-02 20:45:31 -0700

    removed hard-coding of linux to use vulkan legacy, made vulkan legacy it's own waifu2x wrapper.

--- src/dandere2x.py
+++ src/dandere2x.py
@@ -36,14 +36,15 @@ from dandere2xlib.core.difference import difference_loop, difference_loop_resume
 from dandere2xlib.core.merge import merge_loop, merge_loop_resume
 from dandere2xlib.realtime_encoding import run_realtime_encoding
 from dandere2xlib.status import print_status
-from dandere2xlib.utils.dandere2x_utils import valid_input_resolution, get_a_valid_input_resolution, file_exists, get_operating_system
+from dandere2xlib.utils.dandere2x_utils import valid_input_resolution, get_a_valid_input_resolution, file_exists
 from wrappers.dandere2x_cpp import Dandere2xCppWrapper
 from wrappers.ffmpeg.ffmpeg import extract_frames, trim_video
 from wrappers.frame.frame_compressor import compress_frames
 from wrappers.waifu2x.waifu2x_caffe import Waifu2xCaffe
 from wrappers.waifu2x.waifu2x_converter_cpp import Waifu2xConverterCpp
 from wrappers.waifu2x.waifu2x_vulkan import Waifu2xVulkan
-from wrappers.waifu2x.waifu2x_vulkan_linux import Waifu2xVulkanLinux
+from wrappers.waifu2x.waifu2x_vulkan_legacy import Waifu2xVulkanLegacy
+
 
 class Dandere2x:
 
@@ -182,10 +183,7 @@ class Dandere2x:
 
         self.context.logger.info("Threaded Processes Finished successfully")
 
-    # We could just get the name from self.context, but for readibility it's easier to know
-    # What the function does based off what it's passed.
     def get_waifu2x_class(self, name: str):
-
 
         if name == "caffe":
             return Waifu2xCaffe(self.context)
@@ -193,12 +191,11 @@ class Dandere2x:
         elif name == "converter_cpp":
             return Waifu2xConverterCpp(self.context)
 
-        # for the time being linux and vulkan have seperate classes
         elif name == "vulkan":
-            if get_operating_system() == 'linux':
-                return Waifu2xVulkanLinux(self.context)
-
             return Waifu2xVulkan(self.context)
+
+        elif name == "vulkan_legacy":
+            return Waifu2xVulkanLegacy(self.context)
 
         else:
             logging.info("no valid waifu2x selected")
