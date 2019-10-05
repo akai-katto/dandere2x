8d7955c6626d0d5bea53aa5d9c08bee833cc8e17
Tyler <thatweeblife@gmail.com>
2019-10-04 22:34:54 -0700

    organized upscale_folder

--- src/raw_config_gui_upscale_folder_driver.py
+++ src/raw_config_gui_upscale_folder_driver.py
@@ -19,50 +19,38 @@ def main():
 
     start = time.time()
 
-    # get config based on OS
     configfile = os.path.abspath("dandere2x_%s.yaml" % get_operating_system())
 
-    # load yaml
-
+    # We need to make a context object to get variables needed to upscale an entire folder.
     with open(configfile, "r") as read_file:
         config = yaml.safe_load(read_file)
 
     c = Context(config)
-
     input_folder = c.input_folder
     output_folder = c.output_folder
 
-    # for incrementing workspaces
-    iter_val = 1
+    files_in_folder = []
 
     for file in glob.glob(os.path.join(input_folder, "*")):
-        # Create a list of every item in the input_folder
+        files_in_folder.append(os.path.basename(file))
 
-        # load the raw config file again
+    for x in range(len(files_in_folder)):
+
         with open(configfile, "r") as read_file:
             iteration_file = yaml.safe_load(read_file)
 
-        file_name = os.path.join(input_folder, os.path.basename(file))
-        output_name = os.path.join(output_folder, "upscaled_" + os.path.basename(file))
+        file_name = os.path.join(input_folder, files_in_folder[x])
+        output_name = os.path.join(output_folder, "upscaled_" + files_in_folder[x])
 
-        print("workspace is " + c.workspace + str(iter_val) + os.path.sep)
-        print("file name is" + file_name)
-        print("output name is" + output_name)
-
-        # change the needed variables for d2x to work, particularly, workspace is now put into workspace + iter_val
-        # rather than just workspace.
         iteration_file['dandere2x']['usersettings']['input_file'] = file_name
         iteration_file['dandere2x']['usersettings']['output_file'] = output_name
-        iteration_file['dandere2x']['developer_settings']['workspace'] = c.workspace + str(iter_val) + os.path.sep
-
-        print("iter file " + iteration_file['dandere2x']['developer_settings']['workspace'])
-        iter_val = iter_val + 1
+        iteration_file['dandere2x']['developer_settings']['workspace'] = c.workspace + str(x) + os.path.sep
 
         d2x = Dandere2x_Gui_Wrapper(iteration_file)
         d2x.start()
 
-
     print("\n Total runtime duration:", time.time() - start)
+
 
 if __name__ == "__main__":
     main()
