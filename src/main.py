7e62d2cf615ca322d8804695023150db28560e75
Tyler <thatweeblife@gmail.com>
2020-02-19 06:00:10 -0800

    added debug function to main for development

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import argparse
-import sys
-import time
 import os
 import shutil
+import sys
+import time
 
 import yaml
+
 from context import Context
 from dandere2x import Dandere2x
-
 from dandere2xlib.utils.dandere2x_utils import get_operating_system, wait_on_file, dir_exists, file_exists
 from wrappers.dandere2x_wrappers.dandere2x_gui_upscale_folder_wrapper import Dandere2xUpscaleFolder
 
@@ -42,6 +42,7 @@ def load_parser():
     args = parser.parse_args()
     return args
 
+
 def cli_start(args):
     """
     Start Dandere2x using command line
@@ -67,7 +68,7 @@ def cli_start(args):
     config['dandere2x']['usersettings']['scale_factor'] = args.scale_factor
     config['dandere2x']['usersettings']['denoise_level'] = args.noise_level
 
-    print("arg input file: "  + args.input_file)
+    print("arg input file: " + args.input_file)
     if os.path.isdir(args.input_file):
         print("is not dir")
         if not os.path.isdir(args.output_file):
@@ -92,7 +93,7 @@ def cli_start(args):
             except PermissionError:
                 print("Trying to delete workspace via RM tree threw PermissionError - Dandere2x may not work.")
 
-            while(file_exists(context.workspace)):
+            while (file_exists(context.workspace)):
                 time.sleep(1)
 
         d2x = Dandere2x(context)
@@ -110,16 +111,45 @@ def start_gui():
     print("gui start called")
     gui_start()
 
+
+def debug_start():
+    """
+    Debug function meant for dandere2x development. Starts dandere2x with minimal exterior function calls and
+    will only work based off what's in the yaml.
+    """
+    # get config based on OS
+    configfile = "dandere2x_%s.yaml" % get_operating_system()
+
+    # load yaml
+
+    with open(configfile, "r") as read_file:
+        config = yaml.safe_load(read_file)
+
+    # load the context with yaml stuff
+    context = Context(config)
+
+    # continue d2x
+    d2x = Dandere2x(context)
+    d2x.run_concurrent()
+
+
 def main():
     """
     Start a Dandere2x session either through CLI or GUI. Times the session used in either case.
 
     :return:
     """
+
     args = load_parser()
     start = time.time()
 
-    if len(sys.argv) == 1:
+    debug = False
+
+    if debug:
+        """Switch to true for debugging. Not really used otherwise."""
+        debug_start()
+        return
+    elif len(sys.argv) == 1:
         start_gui()
     else:
         cli_start(args)
