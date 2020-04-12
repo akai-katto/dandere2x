07bb41351db43d465a6789cf1b71aced9aa9433b
aka_katto <thatweeblife@gmail.com>
2020-04-11 19:14:30 -0700

    first commit for rework, included remade Frame class

---
+++ cpp_rework/frame/Frame.h
@@ -0,0 +1,39 @@
+
+/*
+    This file is part of the Dandere2x project.
+
+    Dandere2x is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Dandere2x is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/* ========= Copyright aka_katto 2018, All rights reserved. ============
+Original Author: aka_katto 
+Date: 4/11/20 
+Purpose: This class is to facilitate all the functions and uses dandere2x
+         will need to use when handling images. This wrapper
+         wraps around stb_image, which is really portable (but not friendly
+         for dandere2x computations), and is inspired by Princeton's
+         image API library:
+         https://introcs.cs.princeton.edu/java/stdlib/javadoc/Picture.html
+
+   ===================================================================== */
+
+#ifndef CPP_REWORK_FRAME_H
+#define CPP_REWORK_FRAME_H
+
+class Frame {
+
+};
+
+
+#endif //CPP_REWORK_FRAME_H
