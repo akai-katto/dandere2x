7abce025afd919d454baac5694c5210302641903
Tyler <akai.katto@gmail.com>
2021-09-24 20:12:55 -0700

    optimized noising of images to make noising images faster

--- src/dandere2x/dandere2xlib/wrappers/ffmpeg/progressive_noise_adder.py
+++ src/dandere2x/dandere2xlib/wrappers/ffmpeg/progressive_noise_adder.py
@@ -10,7 +10,6 @@ from dandere2x.dandere2xlib.wrappers.ffmpeg.ffmpeg import apply_noise_to_image
 class ProgressiveNoiseAdder(threading.Thread):
 
     def __init__(self, extracted_frames_dir: str, noised_frames_dir: str, frame_count):
-
         super().__init__()
         self.extracted_frames_dir = extracted_frames_dir
         self.noised_frames_dir = noised_frames_dir
@@ -22,16 +21,23 @@ class ProgressiveNoiseAdder(threading.Thread):
     def join(self, timeout=None):
         threading.Thread.join(self, timeout)
 
-    def run(self):
+    def _noise_image_sub_thread(self, frame_number: int):
+        extracted_image = self.extracted_frames_dir + "frame%s.png" % frame_number
+        noise_extracted_image_temp = self.noised_frames_dir + "temp%s.png" % frame_number
+        noise_extracted_image = self.noised_frames_dir + "frame%s.png" % frame_number
 
+        apply_noise_to_image(ffmpeg_dir=self.ffmpeg_path,
+                             input_image=extracted_image,
+                             output_file=noise_extracted_image_temp)
+
+        rename_file_wait(noise_extracted_image_temp, noise_extracted_image)
+
+    def run(self):
         for count in range(1, self.frame_count + 1):
             extracted_image = self.extracted_frames_dir + "frame%s.png" % count
-            noise_extracted_image_temp = self.noised_frames_dir + "temp%s.png" % count
-            noise_extracted_image = self.noised_frames_dir + "frame%s.png" % count
 
             wait_on_file(extracted_image)
-            apply_noise_to_image(ffmpeg_dir=self.ffmpeg_path,
-                                 input_image=extracted_image,
-                                 output_file=noise_extracted_image_temp)
 
-            rename_file_wait(noise_extracted_image_temp,noise_extracted_image)
+            noise_sub_thread = threading.Thread(target=self._noise_image_sub_thread, args=(count,))
+            noise_sub_thread.start()
+
