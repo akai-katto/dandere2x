ca2a7cdcbecdc59f3a666d683b2fc0e0e0796488
Tyler <akai.katto@gmail.com>
2022-10-23 12:31:32 -0700

    refractoring and cleaning up code for progressive frame extractor

--- src/dandere2x/dandere2xlib/wrappers/ffmpeg/progressive_frame_extractor/__init__.py
+++ src/dandere2x/dandere2xlib/wrappers/ffmpeg/progressive_frame_extractor/__init__.py
@@ -2,11 +2,12 @@ from pathlib import Path
 
 from dandere2x.dandere2xlib.utils.dandere2x_utils import rename_file_wait
 from dandere2x.dandere2xlib.utils.yaml_utils import load_executable_paths_yaml
-from dandere2x.dandere2xlib.wrappers.ffmpeg.ffmpeg import apply_noise_to_image
-from dandere2x.dandere2xlib.wrappers.ffmpeg.ffmpeg_progressive_frame_extractor import VideoFrameExtractor
+from dandere2x.dandere2xlib.wrappers.ffmpeg.progressive_frame_extractor._ffmpeg_video_frame_extractor import \
+    FFMpegVideoFrameExtractor
 from dandere2x.dandere2xlib.wrappers.ffmpeg.ffprobe import get_width_height
 
-class ProgressiveFramesExtractorFFMPEG:
+
+class ProgressiveFrameExtractor:
     """
     Temporally extract frames from a video each time next_frame is called.
     Saves into dandere2x's inputs DIR.
@@ -19,7 +20,6 @@ class ProgressiveFramesExtractorFFMPEG:
                  compressed_quality: int,
                  block_size: int,
                  output_options_original: dict):
-
         ffprobe_path = load_executable_paths_yaml()['ffprobe']
         ffmpeg_path = load_executable_paths_yaml()['ffmpeg']
 
@@ -30,28 +30,16 @@ class ProgressiveFramesExtractorFFMPEG:
         self.compressed_quality = compressed_quality
 
         width, height = get_width_height(ffprobe_dir=ffprobe_path, input_video=input_video)
-        self.cap = VideoFrameExtractor(Path(ffmpeg_path), Path(input_video), width, height, block_size, output_options_original)
+        self.cap = FFMpegVideoFrameExtractor(Path(ffmpeg_path), Path(input_video), width, height, block_size,
+                                             output_options_original)
 
         self.ffmpeg_path = load_executable_paths_yaml()['ffmpeg']
 
         self.count = 1
 
     def extract_frames_to(self, stop_frame: int):
-
         for x in range(1, stop_frame):
             self.next_frame()
-
-    def release_capture(self):
-        pass
-        # #todo, investigate / remove this try catch block with an actual solution
-        # try:
-        #     # Closes all the frames
-        #     self.cap.release()
-        #     cv2.destroyAllWindows()
-        #
-        # except cv2.error:
-        #     print("cv2 error caught - this behaviour is unexpected by the developer, but testing to see if this is"
-        #           " a potential duct-tape fix.")
 
     # todo, need to find a fix for "stuck at 99%" error, or getting stuck prematurely.
     def next_frame(self):
